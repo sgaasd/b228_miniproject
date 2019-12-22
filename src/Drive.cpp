ef485748bbc139f001fae1e7b2be403ef978f225
sgaasd <sgaasd19@student.aau.dk>
2019-12-22 10:08:30 +0100

    tilføjet sub og pub

--- src/Drive.cpp
+++ src/Drive.cpp
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 
     ros::NodeHandle n;
 
-    cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+    ros::Subscriber miniproject_sub = n.subscribe("miniproject", 1, drive);
 
-    //Kald på Drive 
+    cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
      
     ros::spin();
 
@@ -36,7 +36,7 @@ void drive(/*parametre fra safety*/){
         loop_rate.sleep();
         }
     } else if (Safety==true){
-        for(int i=0; i<=42; i++){
+        /*for(int i=0; i<=42; i++){
         cmd_vel_pub.publish(DriveMsg(0.0, -0.2));
         loop_rate.sleep();
         }
@@ -44,6 +44,45 @@ void drive(/*parametre fra safety*/){
         cmd_vel_pub.publish(DriveMsg(0.2, 0.0));
         loop_rate.sleep();
         }
+        */
+       switch (/*safety parameter*/){
+                    /*If the sensor on the left side is pressed the robot 
+                    will first drive backwards and turn right*/
+                    case 0:
+                    for(int i=0; i<=42; i++){ 
+                        cmd_vel_pub.publish(DriveMsg(-0.2, 0.0));
+                        loop_rate.sleep();
+                    }
+                    for(int i=0; i<=42; i++){ 
+                        cmd_vel_pub.publish(DriveMsg(0.0, -(0.75)));
+                        loop_rate.sleep();
+                    }
+                    break;
+                    /*If the sensor on the middle is activated the robot 
+                    will drive backwards and turn left*/
+                    case 1:
+                    for(int i=0; i<=42; i++){ 
+                        cmd_vel_pub.publish(DriveMsg(-0.2, 0.0));
+                        loop_rate.sleep();
+                    }
+                    for(int i=0; i<=42; i++){
+                        cmd_vel_pub.publish(DriveMsg(0.0, (0.75)));
+                        loop_rate.sleep();
+                    }
+                    break;
+                    /*If the sensor on the right side is pressed the robot 
+                    will drive backwards and turn left*/
+                     case 2:
+                    for(int i=0; i<=42; i++){ 
+                        cmd_vel_pub.publish(DriveMsg(-0.2, 0.0));
+                        loop_rate.sleep();
+                    }
+                    for(int i=0; i<=42; i++){
+                        cmd_vel_pub.publish(DriveMsg(0.0, (0.75)));
+                        loop_rate.sleep();
+                    }
+                    break;
+                }
     }
 
 }
--- src/Safety.cpp
+++ src/Safety.cpp
@@ -1,19 +1,11 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/CliffEvent.h>
 #include <kobuki_msgs/BumperEvent.h>
+#include "std_msgs/String.h"
 using namespace std;
 
 /*Publisher varible is initialised*/
-ros::Publisher cmd_vel_pub;
-
-/*A function for defining cmd_vel_message is created*/
-geometry_msgs::Twist SafetyMsg(float x, float z){
-    geometry_msgs::Twist cmd_vel_message;
-    cmd_vel_message.angular.z = z;
-    cmd_vel_message.linear.x = x;
-    return cmd_vel_message;
-}
+ros::Publisher miniproject_pub;
 
 /*A class called Safety_CallBack, that contains the different functions, 
 is created*/
@@ -21,109 +13,21 @@ class Safety_CallBack {
     public: 
         /*The function for the Cliff sensor is created. 
         With poniter that passes data from each sensor and its state*/
-        void CliffCallback(const kobuki_msgs::CliffEvent::ConstPtr& msg){
-        geometry_msgs::Twist cmd_vel_message;
+    void CliffCallback(const kobuki_msgs::CliffEvent::ConstPtr& msg){
         bool cliffs = msg->state;
         int sensors = msg->sensor;
-        /*The robot has 2 second for each state. */ 
-        ros::Rate loop_rate(21);
-            /*The robot should make different turns depending on which sensor 
-            is activated*/
-            if (cliffs == 1) {
-                ROS_INFO("The cliff sensr on the robot has been activated");
-                switch (sensors){
-                    /*If the sensor on the left side is pressed the robot 
-                    will first drive backwards and turn right*/
-                    case 0:
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(-0.2, 0.0));
-                        loop_rate.sleep();
-                    }
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(0.0, -(0.75)));
-                        loop_rate.sleep();
-                    }
-                    break;
-                    /*If the sensor on the middle is activated the robot 
-                    will drive backwards and turn left*/
-                    case 1:
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(-0.2, 0.0));
-                        loop_rate.sleep();
-                    }
-                    for(int i=0; i<=42; i++){
-                        cmd_vel_pub.publish(SafetyMsg(0.0, (0.75)));
-                        loop_rate.sleep();
-                    }
-                    break;
-                    /*If the sensor on the right side is pressed the robot 
-                    will drive backwards and turn left*/
-                     case 2:
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(-0.2, 0.0));
-                        loop_rate.sleep();
-                    }
-                    for(int i=0; i<=42; i++){
-                        cmd_vel_pub.publish(SafetyMsg(0.0, (0.75)));
-                        loop_rate.sleep();
-                    }
-                    break;
-                }
-            }
-               
-        }
-        /*The function BumperCallBack tells if the bumper is pressed 
-        and which side is pressed*/
-        void BumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
-        geometry_msgs::Twist cmd_vel_message;
+
+
+    }
+    /*The function BumperCallBack tells if the bumper is pressed 
+    and which side is pressed*/
+    void BumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
         bool hit = msg->state;
         int bump = msg->bumper;
-        ros::Rate loop_rate(21);
-        if(hit == 1) {
-            ROS_INFO("The bumper on the robot has been hit");
-            /*The robot should make different turns depending on 
-            which bumper is pressed*/
-            switch (bump){
-                /*If the bumper on the left side is pressed the robot 
-                will first drive backwards and turn right*/
-                case 0:
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(-0.2, 0.0));
-                        loop_rate.sleep();
-                    }
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(0.0, -(0.75)));
-                        loop_rate.sleep();
-                    }
-                    break;
-                /*If the bumper on the middle is pressed the robot 
-                will drive backwards and turn left*/
-                case 1:
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(-0.2, 0.0));
-                        loop_rate.sleep();
-                    }
-                    for(int i=0; i<=42; i++){
-                        cmd_vel_pub.publish(SafetyMsg(0.0, (0.75)));
-                        loop_rate.sleep();
-                    }
-                    break;
-                /*If the bumper on the right side is pressed the robot 
-                will drive backwards and turn left*/
-                case 2:
-                    for(int i=0; i<=42; i++){ 
-                        cmd_vel_pub.publish(SafetyMsg(-0.2, 0.0));
-                        loop_rate.sleep();
-                    }
-                    for(int i=0; i<=42; i++){
-                        cmd_vel_pub.publish(SafetyMsg(0.0, (0.75)));
-                        loop_rate.sleep();
-                    }
-                    break;
-            }
-        }
-           
+
+
     }
+        
     private:
 };
 
@@ -133,10 +37,6 @@ int main(int argc, char *argv[]){
     ros::init(argc, argv, "Safety");
     ros::NodeHandle n;
     
-    /*cmd_vel_pub is defined*/
-    cmd_vel_pub = n.advertise<geometry_msgs::Twist>
-    ("/cmd_vel_mux/input/safety_controller", 1);
-
     /*To call the class it needs to be declared*/
     Safety_CallBack safetyClass;
     
@@ -151,6 +51,8 @@ int main(int argc, char *argv[]){
     topic "mobile_base/events/bumper" run "BumperCallBack" */
     ros::Subscriber Bumper_sub = n.subscribe("mobile_base/events/bumper",
      1, &Safety_CallBack::BumperCallback, &safetyClass);
+
+     ros::Publisher miniproject_pub = n.advertise<std_msgs::String>("miniproject", 1);
 
     /*The program has run continuously */
     ros::spin();
